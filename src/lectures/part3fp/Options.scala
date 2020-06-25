7070bd2ed990aa93f4b10ea10e6090f831ba2dd9
Jhony Vidal <joliveira@sms-latam.com>
2020-06-24 23:35:07 -0300

    Complete Option Lecture and use of functions to handle Options[A]

--- src/lectures/part3fp/Options.scala
+++ src/lectures/part3fp/Options.scala
@@ -1,9 +1,14 @@
 package lectures.part3fp
 
+import scala.util.Random
+
 object Options extends App {
 
   /**
    * NOTES
+   *
+   * Use Options to avoid endless amount of null-related assertions
+   * avoid runtime crashes due to NullPointerExceptions(NPEs)/Boogeyman
    *
    *  An Option is a wrapper for a value that might be present or not
    *  sealed abstract class Option[+A]
@@ -46,4 +51,71 @@ object Options extends App {
   println(myFirstOption.map(_ * 2))
   println(myFirstOption.filter(x => x > 10))
   println(myFirstOption.flatMap(x => Option(x * 10)))
+
+  // for-comprehension
+  /**
+   * Exercise
+   */
+  val config: Map[String, String] = Map(
+    // fetched from elsewhere
+    "host" -> "176.45.36.1",
+    "port" -> "90"
+  )
+
+  class Connection {
+    def connect = "Connected" // connect to some server
+  }
+  object Connection {
+    val random = new Random(System.nanoTime())
+    def apply(host: String, port: String): Option[Connection] =
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+
+  // try to establish a connection, if so - print the connect method
+  val host = config.get("host")
+  val port = config.get("port")
+
+  val connection = host.flatMap(h => port.flatMap(p => Connection.apply(h, p))) /// ---> returns a Option[Connection]
+  /**
+   * Imperative way
+   * Logic here is that the val connection = x.flatmap... replace the below logic
+   * if (h ! = null
+   *  if (p != null)
+   *    return Connection.apply(h,p)
+   *  return null
+   */
+
+  val connectionStatus = connection.map(c => c.connect)
+  /**
+   * Imperative way
+   * if (c != null)
+   *  return c.connect
+   * return null
+    */
+
+  println(connectionStatus)
+  /// if (connectionStatus == null) println(None) else print (Some(connectionStatus.get))
+
+  connectionStatus.foreach(println)
+  /**
+   * Imperative way
+   * if (status != null )
+   *  println(status)
+   */
+
+  /// Another solution is the chained calls solution
+  config.get("host")
+    .flatMap(host => config.get("port")
+      .flatMap(port => Connection(host, port))
+      .map(connection => connection.connect))
+    .foreach(println)
+
+  // for-comprehension
+  val forConnectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+  forConnectionStatus.foreach(println)
 }
